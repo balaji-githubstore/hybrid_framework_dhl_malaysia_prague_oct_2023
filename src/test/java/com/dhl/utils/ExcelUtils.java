9fe01f0b08a7180266263a189d135322d257a872
Balaji <balaji0017@gmail.com>
2023-11-01 06:32:29 +0000

    excel to two dim array code added

--- src/test/java/com/dhl/utils/ExcelUtils.java
+++ src/test/java/com/dhl/utils/ExcelUtils.java
@@ -1,28 +1,24 @@
-package com.dhl.test;
+package com.dhl.utils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ZDemo2 {
-
-	public static void main(String[] args) throws IOException {
-
-		FileInputStream file = new FileInputStream("test_data/OrangeData.xlsx");
-
+public class ExcelUtils {
+	
+	public static String[][] getSheetIntoTwoDimensionalArray(String filePath,String sheetname) throws IOException
+	{
+		FileInputStream file = new FileInputStream(filePath);
 		XSSFWorkbook book = new XSSFWorkbook(file);
-
-		XSSFSheet sheet = book.getSheet("invalidLoginTest");
+		XSSFSheet sheet = book.getSheet(sheetname);
 		
 		int rowCount=sheet.getPhysicalNumberOfRows();
 		int colCount=sheet.getRow(0).getPhysicalNumberOfCells();
 
 		String[][] data=new String[rowCount-1][colCount];
-	
 		DataFormatter format=new DataFormatter();
 		
 		for(int r=1;r<rowCount;r++)
@@ -33,7 +29,9 @@ public class ZDemo2 {
 			}
 		}
 		
-		System.out.println(data);
+		book.close();
+		file.close();
+		return data;
 	}
 
 }
