c31376d5106d33bbdfd4919317a66190a381f576
Balaji <balaji0017@gmail.com>
2023-11-01 07:45:41 +0000

    Created WebDriverKeywords

---
+++ src/test/java/com/dhl/base/WebDriverKeywords.java
@@ -0,0 +1,13 @@
+package com.dhl.base;
+
+import org.openqa.selenium.WebDriver;
+
+public class WebDriverKeywords {
+
+	private WebDriver driver;
+
+	public WebDriverKeywords(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}
